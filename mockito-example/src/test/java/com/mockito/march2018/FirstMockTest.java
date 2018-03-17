e3ffb627e264bd2800a9ae2a37bbc75d751dfbdc
abhishek <abhiwanttochat@gmail.com>
2018-03-17 23:37:22 +0530

    Initial commit of Mockito

---
+++ mockito-example/src/test/java/com/mockito/march2018/FirstMockTest.java
@@ -0,0 +1,15 @@
+package com.mockito.march2018;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstMockTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
