19b33fab2e3bafdf59e522d2fb43d0375aef95ac
abhishek <abhiwanttochat@gmail.com>
2018-03-19 01:03:11 +0530

    Service and BusinessImpl add

---
+++ mockito-example/src/main/java/com/mockito/business/TodoBusinessImpl.java
@@ -0,0 +1,30 @@
+package com.mockito.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mockito.data.api.TodoService;
+
+//TodoBusinessImpl is SUT: System Under Test
+//TodoService is Dependency
+public class TodoBusinessImpl {
+
+	private TodoService todoService;
+
+	public TodoBusinessImpl(TodoService todoService) {
+		this.todoService = todoService;
+	}
+
+	private List<String> retrieveTodoRelatedToSpring(String user) {
+		List<String> filteredTodos = new ArrayList<>();
+		List<String> todos = todoService.retrieveToDos(user);
+		for(String todo: todos) {
+			if(todo.contains("Spring")) {
+				filteredTodos.add(todo);
+			}
+		}
+		
+		return filteredTodos;
+	}
+
+}
