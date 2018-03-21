88375fc3ffd8dff21b90859fe5b152b8ed845667
abhishek <abhiwanttochat@gmail.com>
2018-03-21 09:37:51 +0530

    test todoService using Mock

---
+++ mockito-example/src/test/java/com/mockito/business/TodoBusinessImplMockTest.java
@@ -0,0 +1,30 @@
+package com.mockito.business;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.mockito.data.api.TodoService;
+import com	.mockito.data.api.TodoServiceStub;
+
+public class TodoBusinessImplMockTest {
+
+	@Test
+	public void testRetrieveTodoRelatedToSpring_usingMock() {
+		TodoService todoServiceMock = mock(TodoService.class);
+		List<String> todos = Arrays.asList("Learn Spring MVC","Learn Spring","Learn to dance");
+		when(todoServiceMock.retrieveToDos("Dummy")).thenReturn(todos);
+		
+		//TodoService todoServiceStub = new TodoServiceStub();
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		List<String> filteredTodos = todoBusinessImpl.retrieveTodoRelatedToSpring("Dummy");
+		assertEquals(2, filteredTodos.size());
+	}
+
+}
