f70691ba7b82e4686a96003a0b2e47dc78a21df9
abhishek <abhiwanttochat@gmail.com>
2018-03-22 00:03:37 +0530

    mock List size method

---
+++ mockito-example/src/test/java/com/mockito/business/ListTest.java
@@ -0,0 +1,20 @@
+package com.mockito.business;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class ListTest {
+
+	@Test
+	public void mockListSizeMethod() {
+		List listMock = mock(List.class);
+		when(listMock.size()).thenReturn(2);
+		assertEquals(2,listMock.size());
+	}
+
+}
